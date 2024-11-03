2b1da0897cb534cb1c6790be16ef87faff9445e9
Sabid Rahman <136883738+sabid02@users.noreply.github.com>
2024-11-04 00:31:54 +0600

    pin a location according to google map

--- client/src/components/MyMapComponent.jsx
+++ client/src/components/MyMapComponent.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // Default icon setup for markers
 delete L.Icon.Default.prototype._getIconUrl;
@@ -13,39 +13,54 @@ L.Icon.Default.mergeOptions({
   shadowUrl: markerShadow,
 });
 
-const positions = [
-  { lat: 23.806, lng: 90.368, description: "Mirpur 1, Dhaka" },
-  { lat: 23.81, lng: 90.412, description: "Banani, Dhaka" },
-  { lat: 23.78, lng: 90.279, description: "Dhamrai, Dhaka" },
-  { lat: 23.795, lng: 90.404, description: "Mohammadpur, Dhaka" },
-];
-
 const MyMapComponent = () => {
+  const [position, setPosition] = useState(null);
+
+  // Example locations array
+  const locations = [
+    { lat: 23.800825, lng: 90.356745, description: "Mirpur 1, Dhaka" },
+    { lat: 23.81, lng: 90.412, description: "Banani, Dhaka" },
+    { lat: 23.78, lng: 90.279, description: "Dhamrai, Dhaka" },
+    { lat: 23.795, lng: 90.404, description: "Mohammadpur, Dhaka" },
+  ];
+
+  useEffect(() => {
+    // Get the user's current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setPosition({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  if (!position) {
+    return <div>Loading...</div>; // Display loading message while waiting for location
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
+    <MapContainer
+      center={position}
+      zoom={12}
+      style={{ height: "80vh", width: "80vw" }}
     >
-      <MapContainer
-        center={[23.806, 90.368]}
-        zoom={12}
-        style={{ height: "80vh", width: "80vw" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {positions.map((position, index) => (
-          <Marker key={index} position={[position.lat, position.lng]}>
-            <Popup>{position.description}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </div>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>Your Current Location</Popup>
+      </Marker>
+      {locations.map((loc, index) => (
+        <Marker key={index} position={[loc.lat, loc.lng]}>
+          <Popup>{loc.description}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 };
 
